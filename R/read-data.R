a401a12c7528cad2adb45f1d77cc3c6556d5c5cb
kbrevoort <ken@brevoort.com>
2019-02-16 15:39:43 -0500

    Add function to import transmittal file
    Also adds the id_rssd to the disclosure data by reading and merging the transmittal file.

--- R/read-data.R
+++ R/read-data.R
@@ -1,3 +1,42 @@
+#' Import Transmittal File
+#'
+#' This function imports the transmittal file for each year of CRA
+#' data.
+#' @param year The year of data to be read (can be 2- or 4-digit years)
+#' @return A tibble of data
+#' @importFrom readr read_fwf fwf_widths cols
+#' @importFrom dplyr mutate
+import_transmittal_file <- function(year) {
+  zip_name <- sprintf('data/zipped/%02dexp_trans.zip', year %% 100)
+  file_list <- unzip(zip_name, list = TRUE)
+
+  unzip(zip_name, files = file_list$Name[[1]]) %>%
+    readr::read_fwf(col_positions = fwf_widths(widths = c(10, 1, 4, 30, 40, 25, 2, 10, 10, 10, 10),
+                                               col_names = c('respondent',
+                                                             'agency',
+                                                             'year',
+                                                             'respondent_name',
+                                                             'respondent_address',
+                                                             'respondent_city',
+                                                             'respondent_state',
+                                                             'respondent_zip',
+                                                             'tax_id',
+                                                             'id_rssd',
+                                                             'assets')),
+                    col_types = cols(respondent = col_integer(),
+                                     agency = col_character(),
+                                     year = col_integer(),
+                                     respondent_name = col_character(),
+                                     respondent_address = col_character(),
+                                     respondent_city = col_character(),
+                                     respondent_state = col_character(),
+                                     respondent_zip = col_character(),
+                                     tax_id = col_character(),
+                                     id_rssd = col_integer(),
+                                     assets = col_integer())) %>%
+    mutate(agency = factor_agency(agency))
+}
+
 #' Import Disclosure File D1-1
 #'
 #' This function will read in the D1-1 disclosure files
@@ -41,70 +80,75 @@ import_disclosure_file_d11 <- function(year) {
                          substring(in_tbl$V1, 32))
   }
 
-  mutate(in_tbl,
-         respondent = substr(V1, 6, 15),
-           agency = factor(substr(V1, 16, 16),
-                           levels = c('1', '2', '3', '4'),
-                           labels = c('OCC', 'FRB', 'FDIC', 'OTS')),
-           year = to_numeric(substr(V1, 17, 20)),
-           loan_type = substr(V1, 21, 21),
-           action = substr(V1, 22, 22),
-           state = to_numeric(substr(V1, 23, 24)),
-           county = to_numeric(substr(V1, 25, 27)),
-           metro_area = to_numeric(substr(V1, 28, 32)),
-           assessment_area = to_numeric(substr(V1, 33, 36)),
-           partial_county_fl = to_logical(substr(V1, 37, 37)),
-           split_county_fl = to_logical(substr(V1, 38, 38)),
-           population_f = factor(substr(V1, 39, 39),
-                                 levels = c('S', 'L', ' '),
-                                 labels = c('<= 500,000 in population',
-                                            '>500,000 in population',
-                                            'Total')),
-           income_group = factor(as.numeric(substr(V1, 40, 42)),
-                                 levels = c(1:15, 101:106),
-                                 labels = c('< 10% of MFI',
-                                            '10% to 20% of MFI',
-                                            '20% to 30% of MFI',
-                                            '30% to 40% of MFI',
-                                            '40% to 50% of MFI',
-                                            '50% to 60% of MFI',
-                                            '60% to 70% of MFI',
-                                            '70% to 80% of MFI',
-                                            '80% to 90% of MFI',
-                                            '90% to 100% of MFI',
-                                            '100% to 110% of MFI',
-                                            '110% to 120% of MFI',
-                                            '>= 120% of MFI',
-                                            'Unknown MFI',
-                                            'Unknown Tract',
-                                            'Low Income',
-                                            'Moderate Income',
-                                            'Middle Income',
-                                            'Upper Income',
-                                            'Unknown Income',
-                                            'Unknown Tract')),
-           report_level = factor(as.numeric(substr(V1, 43, 45)),
-                                 levels = c(4, 6, 8, 10, 20, 30, 40, 50, 60),
-                                 labels = c('Total Inside & Outside Assessment Area (AA)',
-                                            'Total Inside AA',
-                                            'Total Outside AA',
-                                            'State Total',
-                                            'Total Inside AA in State',
-                                            'Total Outside AA in State',
-                                            'County Total',
-                                            'Total Inside AA in County',
-                                            'Total Outside AA in County')),
-           num_le100k = as.numeric(substr(V1, 46, 51)),
-           amt_le100k = as.numeric(substr(V1, 52, 59)),
-           num_100to250k = as.numeric(substr(V1, 60, 65)),
-           amt_100to250k = as.numeric(substr(V1, 66, 73)),
-           num_250to1m = as.numeric(substr(V1, 74, 79)),
-           amt_250to1m = as.numeric(substr(V1, 80, 87)),
-           num_sbl = as.numeric(substr(V1, 88, 93)),
-           amt_sbl = as.numeric(substr(V1, 94, 101)),
-           num_affiliate = as.numeric(substr(V1, 102, 107)),
-           amt_affiliate = as.numeric(substr(V1, 108, 115))) %>%
-    select(-V1)
+  # Read in the transmittal data to get a respondent to id_rssd map
+  idrssd_map <- import_transmittal_file(year) %>%
+    select(respondent, id_rssd)
+
+  ret_dt <- mutate(in_tbl,
+                   respondent = to_numeric(substr(V1, 6, 15)),
+                   agency = factor_agency(substr(V1, 16, 16)),
+                   year = to_numeric(substr(V1, 17, 20)),
+                   loan_type = substr(V1, 21, 21),
+                   action = substr(V1, 22, 22),
+                   state = to_numeric(substr(V1, 23, 24)),
+                   county = to_numeric(substr(V1, 25, 27)),
+                   metro_area = to_numeric(substr(V1, 28, 32)),
+                   assessment_area = to_numeric(substr(V1, 33, 36)),
+                   partial_county_fl = to_logical(substr(V1, 37, 37)),
+                   split_county_fl = to_logical(substr(V1, 38, 38)),
+                   population_f = factor(substr(V1, 39, 39),
+                                         levels = c('S', 'L', ' '),
+                                         labels = c('<= 500,000 in population',
+                                                    '>500,000 in population',
+                                                    'Total')),
+                   income_group = factor(as.numeric(substr(V1, 40, 42)),
+                                         levels = c(1:15, 101:106),
+                                         labels = c('< 10% of MFI',
+                                                    '10% to 20% of MFI',
+                                                    '20% to 30% of MFI',
+                                                    '30% to 40% of MFI',
+                                                    '40% to 50% of MFI',
+                                                    '50% to 60% of MFI',
+                                                    '60% to 70% of MFI',
+                                                    '70% to 80% of MFI',
+                                                    '80% to 90% of MFI',
+                                                    '90% to 100% of MFI',
+                                                    '100% to 110% of MFI',
+                                                    '110% to 120% of MFI',
+                                                    '>= 120% of MFI',
+                                                    'Unknown MFI',
+                                                    'Unknown Tract',
+                                                    'Low Income',
+                                                    'Moderate Income',
+                                                    'Middle Income',
+                                                    'Upper Income',
+                                                    'Unknown Income',
+                                                    'Unknown Tract')),
+                   report_level = factor(as.numeric(substr(V1, 43, 45)),
+                                         levels = c(4, 6, 8, 10, 20, 30, 40, 50, 60),
+                                         labels = c('Total Inside & Outside Assessment Area (AA)',
+                                                    'Total Inside AA',
+                                                    'Total Outside AA',
+                                                    'State Total',
+                                                    'Total Inside AA in State',
+                                                    'Total Outside AA in State',
+                                                    'County Total',
+                                                    'Total Inside AA in County',
+                                                    'Total Outside AA in County')),
+                   num_le100k = as.numeric(substr(V1, 46, 51)),
+                   amt_le100k = as.numeric(substr(V1, 52, 59)),
+                   num_100to250k = as.numeric(substr(V1, 60, 65)),
+                   amt_100to250k = as.numeric(substr(V1, 66, 73)),
+                   num_250to1m = as.numeric(substr(V1, 74, 79)),
+                   amt_250to1m = as.numeric(substr(V1, 80, 87)),
+                   num_sbl = as.numeric(substr(V1, 88, 93)),
+                   amt_sbl = as.numeric(substr(V1, 94, 101)),
+                   num_affiliate = as.numeric(substr(V1, 102, 107)),
+                   amt_affiliate = as.numeric(substr(V1, 108, 115))) %>%
+    left_join(idrssd_map, by = 'respondent') %>%
+    select(-V1, -respondent)
+
+  select(ret_dt, c('id_rssd', names(ret_dt)))
 }
 
 #' Convert Character to Numeric
@@ -141,6 +185,13 @@ to_logical <- function(x) {
          as.logical(NA))
 }
 
+factor_agency <- function(x) {
+  x <- as.character(x)
+
+  factor(x,
+         levels = c('1', '2', '3', '4'),
+         labels = c('OCC', 'FRB', 'FDIC', 'OTS'))
+}
 
 
 
